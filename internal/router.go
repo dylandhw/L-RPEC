59f96ffefad6bdcb56750b3e28a942643cde2587
dylandhw <dylanwilkins2308@gmail.com>
2026-03-21 08:16:56 -0400

    route matching function

---
+++ internal/router.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"strings"
+)
+
+type Route struct {
+	Path     string
+	Upstream string
+}
+
+// deteriministic route matching system that is based on length
+func Match(routes []Route, path string) (string, bool) {
+	var best Route
+	for _, route := range routes {
+		if strings.HasPrefix(path, route.Path) && len(r.Path) > len(best.Path) {
+			best = route
+		}
+	}
+	if best.Upstream == "" {
+		return "", false
+	}
+
+	return best.Upstream, true
+}
