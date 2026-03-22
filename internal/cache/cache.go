d415d0872bbdec97bae32c99e84bdf2a21cad06c
dylandhw <dylanwilkins2308@gmail.com>
2026-03-22 01:36:58 -0400

    get method

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -33,6 +33,12 @@ func NewCache() *Cache {
 	}
 }
 
-func Get(key string) (Entry, bool) {}
+func (c *Cache) Get(key string) (Entry, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
-func Set(key string, entry Entry) {}
+	entry, ok := c.Entries[key]
+	return entry, ok
+}
+
+func (c *Cache) Set(key string, entry Entry) {}
