a805f2bd44f7ccf7200225afc3b52a230ca98f94
dylandhw <dylanwilkins2308@gmail.com>
2026-03-22 01:28:47 -0400

    entry constructor

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -8,12 +8,21 @@ import (
 
 type Entry struct {
 	ResponseBody []byte
-	http.Header
-	StatusCode int
-	ExpiryTime time.Time
+	Headers      http.Header
+	StatusCode   int
+	ExpiryTime   time.Time
 }
 
 type Cache struct {
 	mu      sync.Mutex
 	Entries map[string]Entry
+}
+
+func NewEntry(ResponseBody []byte, Headers http.Header, StatusCode int, ExpiryTime time.Time) *Entry {
+	return &Entry{
+		ResponseBody: ResponseBody,
+		Headers:      Headers,
+		StatusCode:   StatusCode,
+		ExpiryTime:   ExpiryTime,
+	}
 }
