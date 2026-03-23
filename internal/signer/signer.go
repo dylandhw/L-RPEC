7260dbafb05c158d84e3f6f4f9595db91768d12e
dylandhw <dylanwilkins2308@gmail.com>
2026-03-23 02:19:13 -0400

    finish HMAC signing

--- internal/signer/signer.go
+++ internal/signer/signer.go
@@ -3,14 +3,24 @@ package signer
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/hex"
 	"net/http"
+	"strconv"
 	"time"
 )
 
-func SignRequest(r *http.Response, secretKey []byte) {
-	now := time.Now()
-	unixTime := now.Unix()
+func SignRequest(r *http.Request, secretKey []byte) {
+	now := time.Now().Unix()
+	timeString := strconv.FormatInt(now, 10)
 
+	stringToBeHashed := r.Method + r.URL.Path + timeString
 	// hmac hash
-	h := hmac.New(sha256.New, secretKey)
+	hash := hmac.New(sha256.New, secretKey)
+
+	hash.Write([]byte(stringToBeHashed))
+
+	signature := hash.Sum(nil)
+
+	r.Header.Add("X-Timestamp", timeString)
+	r.Header.Add("X-Signature", hex.EncodeToString(signature))
 }
