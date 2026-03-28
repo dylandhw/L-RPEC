b8081aa5a3c1d920e5d294ac7795d86dba75319b
dylandhw <dylanwilkins2308@gmail.com>
2026-03-27 20:58:04 -0400

    vegeta testing

--- metrics/loadgen.go
+++ metrics/loadgen.go
@@ -1,7 +1,15 @@
 package metrics
 
-import "os/exec"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func LoadGeneration() {
 	cmd := exec.Command("vegeta", "attack", "-duration=10s", "-rate=100", "-targest=targets.txt", "|", "vegeta report", ">>", "reports.txt")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("err")
+	}
+	fmt.Println(string(output))
 }
